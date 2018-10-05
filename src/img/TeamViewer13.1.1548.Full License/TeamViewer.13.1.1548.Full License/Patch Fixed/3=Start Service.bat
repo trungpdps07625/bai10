482f09b6c2cd65a2febb5ccd4f8beae9d66682cf
Trung Pham <someonelikeyou1620@gmail.com>
2018-10-05 16:55:22 +0700

    lên

---
+++ src/img/TeamViewer13.1.1548.Full License/TeamViewer.13.1.1548.Full License/Patch Fixed/3=Start Service.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+CLS
+
+net start TeamViewer
+ECHO ======================================
+ECHO Provided by: TienIchMayTinh
+ECHO ======================================
+ECHO Nhan '' Enter '' De Thoat
+ECHO ======================================
+PAUSE
